58505f5c57d64d4a05eda6bf691fcbd7dc1b2d4f
Mikhail Zyranau <mikhail.zyranau@dogowrld.com>
2019-11-04 20:29:39 +0300

    initial commit

---
+++ button.js
@@ -0,0 +1,25 @@
+export default class Button {
+  constructor(obj) {
+    this.id = obj.code;
+    this.first = obj.first;
+    this.second = obj.second;
+    this.type = obj.type;
+    this.button = this.createEl();
+  }
+
+  createEl = () => {
+    const el = document.createElement('div');
+    el.innerText = this.first;
+    el.id = this.id;
+    el.classList.add(this.type, 'button', 'up');
+    if (this.first === 'Space') {
+      el.classList.add('space');
+      el.innerText = '';
+    }
+    return el;
+  };
+
+  render = () => {
+    return this.button;
+  };
+}
